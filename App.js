62d4eb339db39d624de1702a018256abe151ccce
Harsh Panwar <20001003044harsh@dcrustm.org>
2024-03-11 22:47:28 +0530

    Day8 props and state learned

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { StyleSheet, View } from "react-native";
-import Imagestut from "./Components/Imagestut";
-
-
-
-
+import { StyleSheet, View, Text } from "react-native";
+import { Button } from "react-native-web";
 
 export default function App() {
+  const [name, setName] = useState("Harsh");
+
+  const update = () => {
+    setName("Aryan");
+  };
+
   return (
     <View style={styles.container}>
-    <Imagestut/>
-   
+      <Text style={{ backgroundColor: "yellow" }}>
+        This is parent component
+      </Text>
+      <Button
+        style={{
+          width: 50,
+          height: 70,
+          backgroundColor: "blue",
+          padding: 10,
+          marginTop: 5,
+        }}
+        title="Update Name"
+        onPress={update}
+      ></Button>
+
+      <User name={name} />
     </View>
   );
 }
+
+const User = (props) => {
+  console.warn();
+
+  return (
+    <View>
+      <Text style={{ backgroundColor: "red", marginTop: 20 , color:'white' }}>
+        This is child {props.name} component
+      </Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
