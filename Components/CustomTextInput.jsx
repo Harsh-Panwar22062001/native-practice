78bf06bcc55f0be56417e41a4090377a14fbd1e9
Harsh Panwar <20001003044harsh@dcrustm.org>
2024-03-24 17:47:14 +0530

    Basic form created-learned TextInput , UseState , Hide password ,creating fn shhowing details

--- Components/CustomTextInput.jsx
+++ Components/CustomTextInput.jsx
@@ -1,41 +1,89 @@
-import { View, Text, TextInput, StyleSheet  , Button } from "react-native";
-import React  ,{useState} from "react";
+import { View, Text, TextInput, StyleSheet, Button } from "react-native";
+import React, { useState } from "react";
 
 export default function CustomTextInput() {
+  const [name, setName] = useState("");
 
+  const [email, setEmail] = useState("");
 
-const [name , setName] = useState("");
+  const [password, setPassword] = useState("");
 
+  const [display, setDisplay] = useState(false);
+
+  const blankText = () => {
+    setDisplay(false);
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
 
   return (
     <View>
       <Text
         style={{
           fontSize: 20,
-          fontWeight: 90,
+          fontWeight: "bold",
           marginTop: 20,
           color: "blue",
         }}
       >
         {" "}
-        I am Learning TextInput
+        I Am Learning TextInput
       </Text>
-      <Text>Your Name is :{name}</Text>
-      <TextInput placeholder="Enter Your Name" style={styles.container}   value={name}  onChangeText={(text)=>setName(text)} />
 
-      <Button   title = "Clear Text" onPress ={()=>setName("")}   />
+      <TextInput
+        placeholder="Enter Your Name"
+        style={styles.container}
+        value={name}
+        onChangeText={(text) => setName(text)}
+      />
+
+      <TextInput
+        placeholder="Enter Email Address"
+        style={styles.container}
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+
+      <TextInput
+        placeholder="Enter Password"
+        style={styles.container}
+        value={password}
+        secureTextEntry={true}
+        onChangeText={(text) => setPassword(text)}
+      />
+
+      <View>
+        {display ? (
+          <View>
+            <Text style={{ fontSize: 10 }}> Your Name is {name}</Text>
+            <Text style={{ fontSize: 10 }}> Your Email is {email}</Text>
+            <Text style={{ fontSize: 10 }}> Your Password is {password}</Text>
+          </View>
+        ) : null}
+      </View>
+
+      <Button title="Your Output Text" onPress={() => setDisplay(true)} />
+
+      <View style={{ marginTop: 4 }}>
+        <Button
+          title="Clear Text"
+          onPress={() => 
+            blankText() }
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    Color: "Yellow",
-    fontSize: 10,
-    border: "solid 1px black",
-    borderWidth: 30,
-    margin: 40,
+    color: "black",
+    fontSize: 18,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    margin: 10,
     height: 40,
-    weight: 90,
   },
 });
