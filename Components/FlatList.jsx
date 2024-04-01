97eb702dcc3fbd1b9b6381afd6c27d9f910663cb
Harsh Panwar <20001003044harsh@dcrustm.org>
2024-04-01 21:44:42 +0530

    FlatList Learned or map function learned

--- Components/FlatList.jsx
+++ Components/FlatList.jsx
@@ -1,8 +1,8 @@
-import { View, Text, FlatList, StyleSheet } from "react-native";
+import { View, Text, StyleSheet , ScrollView } from "react-native";
 import React from "react";
 
 export default function flatItem() {
-  const list = [
+  const users = [
     {
       id: 1,
       name: "Harsh",
@@ -23,17 +23,73 @@ export default function flatItem() {
       id: 5,
       name: "Harsh",
     },
+
+
+    {
+      id: 6,
+      name: "Akash",
+    },
+    {
+      id: 7,
+      name: "Ritik",
+    },
+    {
+      id: 8,
+      name: "Gunjan",
+    },
+    {
+      id: 9,
+      name: "Harsh",
+    },
+
+    {
+      id: 10,
+      name: "Akash",
+    },
+    {
+      id: 11,
+      name: "Ritik",
+    },
+    {
+      id: 12,
+      name: "Gunjan",
+    },
+    {
+      id: 13,
+      name: "Harsh",
+    },
+    {
+      id: 14,
+      name: "Gunjan",
+    },
+    {
+      id: 15,
+      name: "Harsh",
+    },
+
   ];
 
   return (
     <View>
-      <Text> The List are mention are </Text>
 
-      <FlatList
+ 
+
+    <Text> The List are mention are </Text>   <ScrollView>
+
+
+{
+
+  users.map((items)=><Text  style ={styles.list2}>{items.name}</Text>)
+}
+
+    </ScrollView>
+      
+
+      {/* <FlatList
         data={list}
         renderItem={({ item }) => <Text style={styles.list2}>{item.name}</Text>}
         keyExtractor={() => item.id}
-      />
+      /> */}
     </View>
   );
 }
@@ -46,6 +102,7 @@ const styles = StyleSheet.create({
     color: "white",
     backgroundColor: "black",
     margin: 10,
-    padding: 10,
+    padding: 5,
+    color:'yellow',
   },
 });
